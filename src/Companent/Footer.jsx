28be1dbd0cdcc8fb6782514d0f1d926f6bfee0c7
Massraja02 <massraja625@gmail.com>
2025-06-01 23:44:15 +0530

    Update Footer.jsx

--- src/Companent/Footer.jsx
+++ src/Companent/Footer.jsx
@@ -7,7 +7,7 @@ import { FaInstagram } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa6";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
-import myimg from '../assets/me/homeimg.png;
+import myimg from '../assets/me/homeimg.png';
 
 function Footer() {
   return (
