5e826adf19c499345fb0681e56a53e24f09a3564
Massraja02 <massraja625@gmail.com>
2025-06-01 22:05:42 +0530

    Update Home.jsx

--- src/Companent/Home.jsx
+++ src/Companent/Home.jsx
@@ -65,7 +65,7 @@ function Home() {
 
             <div data-aos="zoom-in" className="col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 homeimgcol">
                 <div className='homeimgdiv'>
-                <img src="././assets/me/homeimg.png" alt=""/>
+                <img src="./assets/me/homeimg.png" alt=""/>
 
                 <img src="src\assets\svg\react-svgrepo-com.svg" alt="" />
                 <img src="src\assets\svg\node-js-svgrepo-com.svg" alt="" />
